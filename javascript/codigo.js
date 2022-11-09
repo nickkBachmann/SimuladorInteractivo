64bc1a147476035c6882bfa673990c8d7e2166a1
Nicolas Bachmann <nbachmann2016@yahoo.com>
2022-11-08 21:33:11 -0500

    Segunda Entrega

--- javascript/codigo.js
+++ javascript/codigo.js
@@ -1,9 +1,28 @@
 var puntos = 300
 puntos = parseInt(300);
 var promociones = 10
+var nombreApellido = ''
+
+class Producto {
+    constructor(id, nombre, precio){
+        this.id = id
+        this.nombre = nombre
+        this.precio = precio
+    }
+}
+
+const producto1 = new Producto (1, 'Leche', 10)
+const producto2 = new Producto (2, 'Pan', 5)
+const producto3 = new Producto (3, 'Tortillas', 5)
+const producto4 = new Producto (4, 'Vino', 50)
+const producto5 = new Producto (5, 'Jamon', 20)
+
+const Productoss = [producto1, producto2, producto3, producto4, producto5]
 
 function iniciarSesion () {
     let nombreApellido = prompt ("Por favor identifíquese con nombre y apellido")
+    alert("¡Bienvenido! " + nombreApellido)
+    seleccionarOperacion()
 }
 
 function verificarPromocion() {
@@ -41,7 +60,8 @@ function recargarPuntos () {
     }
 
 function hacerPedido () {
-    let pregunta3 = prompt("¿Qué producto desea comprar?")
+    Productoss.forEach((producto) => alert(`Nuestros productos son: ${producto.nombre}`)) 
+    let pregunta3 = prompt("¿Qué producto desea comprar? Leche(10$)  Pan(5$) Tortillas(5$) Vino(50$) Jamon(20$)")
     let pregunta4 = prompt("Pickup o Delivery")
         if (pregunta4 == "pickup") {
             alert("El artículo " + pregunta3 + " estará listo en nuestro local en 30 minutos")
@@ -67,7 +87,7 @@ function finalizarSesión () {
 
 function seleccionarOperacion() {
     
-    let op = prompt("¡Bienvenido! Por favor seleccione una opción")
+    let op = prompt("Por favor seleccione una opción: 1-Consultar promocion 2-Consultar Saldo 3-Recargas 4-Realizar Pedidos 5-Contacto 6-Salir")
     switch (op){
         case "1":
             verificarPromocion()
